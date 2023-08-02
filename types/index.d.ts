d548fe6e3f766a686d702e977a4741fe29e0ea27
BuildTools <unconfigured@null.spigotmc.org>
2023-08-02 15:31:08 +0200

    removed TSC type declarations from Git

--- types/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { EncedeusAPI } from "./api";
-export { User } from "./types/user";
-export { Role } from "./types/role";
-export { UpdateUserDto, CreateUserDto, SetPfpError, UpdateUserError, FindUserError, FindUserResponse, CreateUserError, DeleteUserError, UserDeletedError, UserNotFoundError, UsernameTakenError } from "./services/users_service";
-export { RefreshAccessTokenResponse, SignInUserResponse, SignInUserError, SignOutError, UserSignInDto, InvalidRefreshTokenError, WrongPasswordError, WrongEmailOrUsernameError, RefreshAccessTokenError, UsernameOrEmailNotSpecifiedError } from "./services/auth_service";
-export { UpdateRoleDto, UpdateRoleError, FindRoleError, FindRoleResponse, DeleteRoleError, CreateRoleError, RoleDeletedError, RoleNotFoundError, MissingUserPermissionError } from "./services/role_service";
-export { BadRequestError, ResourcePermanentlyDeletedError, ResourceNotFoundError, UnauthorisedError, InternalServerError, ConflictError } from "./services/errors";
